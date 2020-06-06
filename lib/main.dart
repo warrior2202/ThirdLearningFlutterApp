274147e0b30e2748d4456474228987b0e81bfd7e
Manan Shah <43475582+warrior2202@users.noreply.github.com>
2020-06-06 12:36:25 +0530

    dice project starter code

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
+
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
