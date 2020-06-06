3f4c6ccf5dde0caaff50efccf50a55c3c60670bb
Manan Shah <43475582+warrior2202@users.noreply.github.com>
2020-06-06 12:53:00 +0530

    expanded added

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,11 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      children: <Widget>[
+        Expanded(child: Image.asset('images/dice1.png')),
+        Expanded(child: Image(image: AssetImage('images/dice1.png')))
+      ],
+    );
   }
 }
